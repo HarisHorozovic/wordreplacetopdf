009708caf6b2aec1ccf288487ac6b39b5516a836
Haris Horozovic <harishorozovic5@gmail.com>
2020-01-11 22:26:41 +0100

    Removing build folder

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV !== 'development') {
   app.use(express.static(path.join(__dirname, './client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
   });
 }
 
