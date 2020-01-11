b266f190d949bd596cc2576819d0ba10a919874a
Haris Horozovic <harishorozovic5@gmail.com>
2020-01-11 22:38:23 +0100

    Logging the port on server

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = require('./app');
 
 const port = process.env.PORT || 5000;
 
+console.log(process.env.PORT);
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
