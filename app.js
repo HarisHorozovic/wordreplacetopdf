5bea50b4c923fd40d907799a822bdd5b8fe756e0
Haris Horozovic <harishorozovic5@gmail.com>
2020-01-11 21:03:47 +0100

    Few more fixes for the heroku deployment

--- app.js
+++ app.js
@@ -12,10 +12,11 @@ const fileHandleRouter = require('./routes/fileHandle.routes');
 const app = express();
 
 // Heroku build paths setup
-app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
+console.log(path.join(__dirname, './client/build'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 });
 
 if (process.env.NODE_ENV === 'development') {
