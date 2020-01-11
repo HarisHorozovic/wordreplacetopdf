551fc755fcc40c4f51cc8c91cfc70252270a6e5b
Haris Horozovic <harishorozovic5@gmail.com>
2020-01-11 22:14:54 +0100

    Set up proxy again

---
+++ client/build/precache-manifest.6b2bb2b486b42f9e9b0fc7e8c0ae4c5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a7d7a9a3aa116f8267d682f9e75908",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a117630f06a58b0b599",
+    "url": "/static/css/main.2f37393e.chunk.css"
+  },
+  {
+    "revision": "f1a66071d98992d8a7d0",
+    "url": "/static/js/2.d45a09d8.chunk.js"
+  },
+  {
+    "revision": "c648085972f05ef5ee482f9a082a3723",
+    "url": "/static/js/2.d45a09d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a117630f06a58b0b599",
+    "url": "/static/js/main.e34de8fa.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  }
+]);
--- client/src/setProxy.js
+++ client/src/setProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     proxy('/api/v1', {
-      target: 'https://enigmatic-garden-06901.herokuapp.com/',
+      target: 'https://enigmatic-garden-06901.herokuapp.com',
       changeOrigin: true
     })
   );
