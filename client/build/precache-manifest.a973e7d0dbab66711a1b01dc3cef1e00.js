5adc01dc37e0fd80c55970982c79bc9bb2db9fd5
Haris Horozovic <harishorozovic5@gmail.com>
2020-01-12 15:28:06 +0100

    Fix the bug where files do not download on the deployed application

---
+++ client/build/precache-manifest.a973e7d0dbab66711a1b01dc3cef1e00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e0870b6c89057ab0c86e9d873d15b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61fae9a6f5cbf176775a",
+    "url": "/static/css/main.2f37393e.chunk.css"
+  },
+  {
+    "revision": "f1a66071d98992d8a7d0",
+    "url": "/static/js/2.d45a09d8.chunk.js"
+  },
+  {
+    "revision": "c648085972f05ef5ee482f9a082a3723",
+    "url": "/static/js/2.d45a09d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61fae9a6f5cbf176775a",
+    "url": "/static/js/main.075d3133.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  }
+]);
